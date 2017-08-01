d7892a1c0b2ba7fd78b0546d5ea07ea0b7a67598
Molly Lloyd <molly@mapbox.com>
2017-08-01 17:02:00 -0500

    fix bugs in the masking algorithm

--- src/render/tile_mask.js
+++ src/render/tile_mask.js
@@ -1,3 +1,4 @@
+
 const util = require('../util/util');
 const TileCoord = require('../source/tile_coord');
 
@@ -53,65 +54,46 @@ const TileCoord = require('../source/tile_coord');
 
 
 exports.updateTileMasks = function(renderableTiles) {
-    renderableTiles.sort((a, b) => { a.coord.id < b.coord.id });
-    const mask = [];
-    const end = renderableTiles[renderableTiles.length-1];
+    const sortedRenderables = util.objectValues(renderableTiles).sort((a, b) => { return a.coord.isLessThan(b.coord) ? -1 : b.coord.isLessThan(a.coord) ? 1 : 0; });
 
-
-    for (let i = 0; i < renderableTiles.length; i++){
-        const tile =  renderableTiles[i];
-        const childArray = renderableTiles.slice(i+1);
+    for (let i = 0; i < sortedRenderables.length; i++) {
+        const mask = [];
+        const tile =  sortedRenderables[i];
+        const childArray = sortedRenderables.slice(i + 1);
         // Try to add all remaining ids as children. We sorted the tile list
         // by z earlier, so all preceding items cannot be children of the current
         // tile. We also compute the lower bound of the next wrap, because items of the next wrap
         // can never be children of the current wrap.
-        const childrenEnd = lowerBound(
-            childArray,
-            new TileCoord(0, 0, 0, tile.tileCoord.w + 1),
-            (a, b)=>{
-                return a.coord.id < b.id ? -1 : a.coord.id > b.id ? 1 : 0;
-            });
-        computeTileMasks(tile.coord, tile.coord.wrapped(), childArray, childrenEnd, mask);
-    }
-}
 
-function computeTileMasks(rootTile, ref, childArray, end, tileMask){
+        computeTileMasks(tile.coord.wrapped(), tile.coord, childArray, new TileCoord(0, 0, 0, tile.coord.w + 1), mask);
+        tile.setMask(mask);
+    }
+};
+
+function computeTileMasks(rootTile, ref, childArray, lowerBound, mask) {
     // If the reference or any of its children is found in the list, we need to recurse.
-    for (let i = 0; childArray[i] <= end; i++) {
-        childTile = childArray[i];
-        // The current tile is masked out, so we don't need to add them to the mask set.
-        if (ref.id === childTile.coord.id){
+    for (let i = 0; i < childArray.length; i++) {
+        const childTile = childArray[i];
+        if (!childTile.hasData()) continue;
+        if (!childTile.coord.isLessThan(lowerBound)) return;
+        if (ref.id === childTile.coord.id) {
+            // The current tile is masked out, so we don't need to add them to the mask set.
             return;
         } else if (childTile.coord.isChildOf(ref)) {
-            // There's at least one child tile that is masked out, so recursively descend.
-            for (const child in ref.children()) {
-                computeTileMasks(rootTileId, child, childArray, end, mask);
+            // There's at least one child tile that is masked out, so recursively descend
+            const children = ref.children();
+            for (let j = 0; j < children.length; j++) {
+                const child = children[j];
+                computeTileMasks(rootTile, child, childArray.slice(i), lowerBound, mask);
             }
+            return;
         }
     }
-
     // We couldn't find a child, so it's definitely a masked part.
     // Compute the difference between the root tile ID and the reference tile ID, since TileMask
     // elements are always relative (see below for explanation).
-
     const diffZ = ref.z - rootTile.z;
-    mask.push(new TileCoord(diffZ, ref.x - (rootTile.x << diffZ), ref.y - (rootTile.y << diffZ)));
+    const maskTileId = new TileCoord(diffZ, ref.x - (rootTile.x << diffZ), ref.y - (rootTile.y << diffZ)).id;
+    if (mask.indexOf(maskTileId) < 0) mask.push(maskTileId);
 }
 
-function lowerBound(array, value, comparator) {
-    const low = 0, high = array.length - 1;
-    while (lo < high) {
-        const i = high / 2, comp = comparator(array[i], value);
-        if ( comp < 0 ) {
-            high = i - 1;
-        } else if (comp > 0){
-            low = m+1;
-        } else {
-            high = i
-        }
-    }
-    if(comparator(array[low], value) <= 0) {
-      return array[low];
-    }
-    return array[low - 1];
-}
